30add5117efbf263c8dbb40298423863a70e744e
Yili Aiwazian <yili@grandtrunklabs.com>
2014-08-24 21:23:51 -0700

    minor cleanup

--- TipCalculator/MainViewController.swift
+++ TipCalculator/MainViewController.swift
@@ -28,7 +28,6 @@ class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
         tipAmountLabel.text = formatCurrency(tipAmount, withSymbol: true)
         totalAmountLabel.text = formatCurrency(totalAmount, withSymbol: false)
         billCurrencyLabel.text = currencySymbol()
